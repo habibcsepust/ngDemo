3e6f1d05f62e4c08c058dea4e9288982e62b85c1
Habibur Rahman <habibcsepust@gmail.com>
2022-09-14 09:59:31 -0600

    h

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <form>
+      <!-- Property Binding -->
       <input type="text" [value]="person_name"/>
       <input type="text" value="{{person_name}}"/>
       <input type="text" bind-value="person_name"/>
@@ -13,11 +14,17 @@ import { Component } from '@angular/core';
       <input type="button" value="Hidden Button1"/>
       <input type="button" value="Hidden Button2" hidden />
       <input type="button" value="Hidden Button3" [hidden]="isHidden"/>
-
+      <!-- End -->
       <hr/>
-
       Title: {{evilTitle}}<br>
       Title: <span [innerHtml]="evilTitle"></span>
+      <hr/>
+
+      <input type="submit" (click)="onClick()" value="Hit me.." />
+      <button (click)="onClick()">Clcik from button</button>
+      <button on-click="onClick()">Clcik me</button>
+      <hr/>
+      <img class="images" src="images/img.png" alt="Habibur Rahman image"/>
   </form>
   `,
   styleUrls: ['./app.component.css']
@@ -28,4 +35,9 @@ export class AppComponent {
   isDisable = false;
   isHidden = true;
   evilTitle = 'Template <script>alert("Evil never sleep")</script> Syntax';
+
+  onClick(){
+    console.log("Clicked Successfully..!");
+    alert("Clicked");
+  }
 }
